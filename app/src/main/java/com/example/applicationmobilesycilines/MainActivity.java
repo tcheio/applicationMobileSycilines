99631264d0cd9dde1780d206ad4f9febf547b628
tcheio <113595151+tcheio@users.noreply.github.com>
2023-04-08 11:48:30 +0200

    IMPORTATION PC FIXE ET EVENT BOUTON DEBUT

--- app/src/main/java/com/example/applicationmobilesycilines/MainActivity.java
+++ app/src/main/java/com/example/applicationmobilesycilines/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.applicationmobilesycilines;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.transition.Visibility;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.applicationmobile.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button login;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login= (Button) findViewById(R.id.login);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
+
+
 }
